aa9c0615f857d18b22f5056edd7689ec61d110be
Diegobrix <dishi.ata@gmail.com>
2023-11-04 21:10:35 -0300

    Configuração Inicial de Estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormComponent } from './components/form/form.component';
+import { LegendComponent } from './components/legend/legend.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ResultListComponent } from './components/result-list/result-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormComponent,
+    LegendComponent,
+    FooterComponent,
+    ResultListComponent
   ],
   imports: [
     BrowserModule,
